98efe489d400a907828256b369d03b48a673a1bc
BibasYonghang <bibasyonghang100@gmail.com>
2026-01-19 07:56:46 +0545

    chore: update baseline broweser mapping data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/AuthSlice.jsx";
-import usersReducer from "./auth/UserSlice.jsx";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    users: usersReducer,
   },
 });
